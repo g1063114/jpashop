066e6a19aef60350fabe74e178f4d2bfe4ea4f72
jinho <g1063114@naver.com>
2021-06-16 23:00:21 +0900

    Member Entity, Service, Repository create

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)     // 기본적으로 public 메서드에 적용됨. 읽기 전용이라 최적화 알아서 해줌
+@RequiredArgsConstructor            // final 키워드 있는 애들 생성자 자동으로 만들어 줌
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /*
+     * 회원 가입
+     */
+    @Transactional  // 쓰기에는 무조건 transactional 있어야 함
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    
+    // 회원 단일 조회
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
