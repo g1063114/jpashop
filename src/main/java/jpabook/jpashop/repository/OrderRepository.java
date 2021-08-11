bc5363df3a19e08e32bcf2343141ab08a652a179
jinho <g1063114@naver.com>
2021-08-11 13:50:20 +0900

    fetch join

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -93,5 +93,13 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
+
     // 추후에 Querydsl로 바꾼다다
 }
